29b1ff681d7f4748357b9a7247f63b388dbdc7e1
gowripurushothaman <gowrikalpana27@gmail.com>
2017-07-04 05:03:39 +0530

    Create lcp.c

---
+++ lcp.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+char a[10],b[10],i,j;
+int c,d;
+printf("enter the first string");
+gets(a);
+printf(:enter the second string");
+gets(b);
+c=strlen(a);
+d=strlen(b);
+for(i=0;i<c;i++)
+{
+for(j=0;j<d;j++)
+{
+if(a[i]==b[i])
+printf("%S",a[i]);
+}}
+}
